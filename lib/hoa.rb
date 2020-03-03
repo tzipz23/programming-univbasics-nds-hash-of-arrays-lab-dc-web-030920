8b4bf0a153af5642fa979a792969167ba5522c82
TOMAZYE ANDERSON <tomazyeanderson@yahoo.com>
2020-03-03 00:18:42 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -7,7 +7,7 @@ BASE_HOA = {
 
 def add_character(show, name)
   
-  BASE_HOA[:chipmunks] << "Dave"
+  BASE_HOA[:chipmunks][:third_earthers][:jetsons] << "Dave"
 
   
   # Write your implementation here
